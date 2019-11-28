fd0e000b64a2531147832f8856f9a0cfabdf68ef
Long Tran <tran6388@uw.edu>
2019-11-27 19:37:47 -0800

    add more for create

--- newfreebies/src/CreateModal.js
+++ newfreebies/src/CreateModal.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import { db, auth } from './services/firebase';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 
 class CreateModal extends Component {
@@ -11,17 +13,16 @@ class CreateModal extends Component {
             createModalShow: false,
             selectedOption: 'definite',
             title: null,
-            date: null,
             description: null,
             downvotes: null,
-            endTime: null,
             isDefinite: null,
             location: null,
             picture: null,
-            startTime: null,
             tag: null,
             upvotes: null,
-            username: null
+            username: null,
+            startDate: null,
+            endDate: null
         }
 
         this.createOpenModal = this.createOpenModal.bind(this);
@@ -33,7 +34,7 @@ class CreateModal extends Component {
         this.setState({ createModalShow: true })
     }
 
-    createClose = () => this.setState({ createModalShow: false });
+    createClose = () => this.setState({ createModalShow: false, startDate: null, endDate: null });
 
     handleOptionChange = (changeEvent) => {
         this.setState({
@@ -44,53 +45,40 @@ class CreateModal extends Component {
     addNewPost = (event) => {
 
         this.setState(function () {
-            if (this.state.selectedOption === 'indefinite') {
-                return {
-                    title: document.getElementById('inputTitle').value,
-                    location: document.getElementById('inputLocation').value,
-                    description: document.getElementById('inputDescription').value,
-                    isDefinite: this.state.selectedOption,
-                    picture: 'http:/url-to-image.png',
-                    tag: document.getElementById('inputTag').value,
-                    username: 'tran6388@uw.edu',
-                }
-            } else {
-                return {
-                    title: document.getElementById('inputTitle').value,
-                    location: document.getElementById('inputLocation').value,
-                    description: document.getElementById('inputDescription').value,
-                    isDefinite: this.state.selectedOption,
-                    picture: 'http:/url-to-image.png',
-                    tag: document.getElementById('inputTag').value,
-                    username: 'tran6388@uw.edu',
-                    date: document.getElementById('inputDate').value,
-                    endTime: document.getElementById('inputEndTime').value,
-                    startTime: document.getElementById('inputStartTime').value
-                }
+            return {
+                title: document.getElementById('inputTitle').value,
+                location: document.getElementById('inputLocation').value,
+                description: document.getElementById('inputDescription').value,
+                isDefinite: this.state.selectedOption,
+                picture: 'http:/url-to-image.png',
+                tag: document.getElementById('inputTag').value,
+                username: 'tran6388@uw.edu',
             }
+            
         }, function () {
             db.collection('posts')
                 .add({
                     title: this.state.title,
                     location: this.state.location,
-                    date: this.state.date,
                     description: this.state.description,
                     downvotes: '0',
-                    endTime: this.state.endTime,
+                    endDate: this.state.endDate,
                     isDefinite: this.state.isDefinite,
                     picture: 'http:/url-to-image.png',
-                    startTime: this.state.startTime,
+                    startDate: this.state.startDate,
                     tag: this.state.tag,
                     upvotes: '0',
+                    //createAt: new Date('December 17, 1995 03:24:00'),
+                    createAt: new Date(),
                     username: 'tran6388@uw.edu'
                 })
                 .then(res => {
                     console.log(res.id)
                 })
+            this.createClose();
         });
-
-        this.createClose();
     }
+
 
     render() {
         return (
@@ -104,18 +92,43 @@ class CreateModal extends Component {
                             <p><span className="modal-label">Title: </span><input type="text" id="inputTitle" /></p>
                             <p><span className="modal-label">Location: </span><input type="text" id="inputLocation" /></p>
                             <div>
-                                <input type="radio" value="definite" id="inputDefinite" defaultChecked checked={this.state.selectedOption === 'definite'} onChange={this.handleOptionChange} /> Definite
+                                <input type="radio" value="definite" id="inputDefinite" checked={this.state.selectedOption === 'definite'} onChange={this.handleOptionChange} /> Definite
                                 <input type="radio" value="indefinite" id="inputIndefinite" checked={this.state.selectedOption === 'indefinite'} onChange={this.handleOptionChange} /> Indefinite
                                 {this.state.selectedOption === "definite" ?
                                     <div>
-                                        <p><span className="modal-label">Date: </span><input type="text" id="inputDate" /></p>
+                                        {/* <p><span className="modal-label">Date: </span><input type="text" id="inputDate" /></p> */}
+                                        <p><span className="modal-label">Event time: </span></p>
+
                                         <p>
-                                            <span className="modal-label">Time: </span>
-                                            <span className="modal-label">Start: </span>
-                                            <input type="text" id="inputStartTime" />
-                                            <span className="modal-label">End: </span>
-                                            <input type="text" id="inputEndTime" />
+                                            <span className="modal-label">Start Time: </span>
+
+                                            <DatePicker
+                                                selected={this.state.startDate}
+                                                onChange={date => this.setState({ startDate: date })}
+                                                showTimeSelect
+                                                timeFormat="HH:mm"
+                                                timeIntervals={15}
+                                                timeCaption="time"
+                                                dateFormat="MMMM d, yyyy h:mm aa"
+                                                id="inputStartDate"
+                                            />
                                         </p>
+
+                                        <p>
+                                            <span className="modal-label">End Time: </span>
+                                            <DatePicker
+                                                selected={this.state.endDate}
+                                                onChange={date => this.setState({ endDate: date })}
+                                                showTimeSelect
+                                                timeFormat="HH:mm"
+                                                timeIntervals={15}
+                                                timeCaption="time"
+                                                dateFormat="MMMM d, yyyy h:mm aa"
+                                                id="inputEndDate"
+                                            />
+                                        </p>
+
+
                                     </div> : null
                                 }
                             </div>
